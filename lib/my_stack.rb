dbfc1ef67f073a4d31c1adb6adb44b30cfda5c1a
BlueVajra <cory.leistikow@gmail.com>
2014-06-30 16:38:04 -0600

    user can pop elements in MyStack, LIFO order

--- lib/my_stack.rb
+++ lib/my_stack.rb
@@ -1,7 +1,17 @@
+class Node
+  attr_accessor :name, :next
+  def initialize(name, next_node)
+    @name = name
+    @next = next_node
+  end
+end
+
 class MyStack
   def initialize
     @size = 0
+    @head = nil
   end
+
   def empty?
     @size == 0 ? true : false
   end
@@ -12,5 +22,35 @@ class MyStack
 
   def push(item)
     @size += 1
+    if @head == nil
+      @head = Node.new(item, nil)
+    else
+      node = @head
+      until node.next == nil
+        node = node.next
+      end
+      node.next = Node.new(item, nil)
+    end
+  end
+
+  def pop
+    if @head != nil
+      node = @head
+      prev_node = nil
+      until node.next == nil
+        prev_node = node
+        node = node.next
+      end
+      result = node.name
+      if @head.next != nil
+        prev_node.next = nil
+      else
+        @head = nil
+      end
+      @size -= 1
+      result
+    else
+      nil
+    end
   end
 end
--- spec/my_stack_spec.rb
+++ spec/my_stack_spec.rb
@@ -12,4 +12,14 @@ describe MyStack do
     expect(stack.size).to eq 1
     expect(stack.empty?).to eq false
   end
+  it "allows user to pop item off the end of the stack" do
+    stack = MyStack.new
+    stack.push("one")
+    stack.push("two")
+    expect(stack.size).to eq 2
+    expect(stack.pop).to eq "two"
+    expect(stack.size).to eq 1
+    expect(stack.pop).to eq "one"
+    expect(stack.size).to eq 0
+  end
 end
